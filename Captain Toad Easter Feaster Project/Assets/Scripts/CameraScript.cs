2195e163e906ce834d71b20f748ff556b6fc24c1
BriekKeters <briek.keters@student.howest.be>
2019-09-30 12:33:23 +0200

    camera

---
+++ Captain Toad Easter Feaster Project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Vector2 LimitY;
+    private float Speed = 60f;
+    float RotationY, RotationX;
+    private int _zoomState;
+    public int ZoomUnits;
+    private Vector3 _forwardPosition;
+    private bool _followToad;
+    public Transform Toad;
+    public Vector3 CenterOfLevel;
+    public float CameraSpeed;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Zoom();
+        transform.position = Vector3.Lerp(transform.position, _forwardPosition, Time.deltaTime*10);
+        if (_followToad)
+        {
+            transform.position = Toad.position+_forwardPosition;
+            CenterOfLevel = Toad.position;
+
+        }
+        else
+        {
+            CenterOfLevel = Vector3.zero;
+            transform.position = Vector3.zero;
+            //camera.main.transform.position.Lookat(CenterofLevel)
+        }
+
+        Debug.Log(Vector3.Normalize(Camera.main.transform.position - Toad.position));
+    }
+
+    private void Zoom()
+    {
+        if (Input.GetButtonDown("Zoom"))
+        {
+
+            if (_zoomState < 1)
+            {
+                _zoomState++;
+            }
+            else _zoomState = 0;
+            if (_zoomState == 1)
+            {
+                _followToad = true;
+                _forwardPosition =   (Toad.position-Camera.main.transform.position)-Vector3.Normalize(Toad.position - Camera.main.transform.position)*3;
+
+            }
+            else
+            {
+                _followToad = false;
+                _forwardPosition = Vector3.zero;
+
+            }
+
+
+        }
+
+    }
+
+    private void Rotate()
+    {
+        //RotationX += Input.GetAxis("VerticalRight") * Speed * Time.deltaTime;
+        // RotationY += Input.GetAxis("HorizontalRight") * Speed * Time.deltaTime;
+        Camera.main.transform.RotateAround(CenterOfLevel, Vector3.up, Input.GetAxis("VerticalRight") * -1 * Time.deltaTime * CameraSpeed);
+        Camera.main.transform.RotateAround(CenterOfLevel, Camera.main.transform.right, Input.GetAxis("HorizontalRight") * -1 * Time.deltaTime * CameraSpeed);
+
+        RotationY = Mathf.Clamp(RotationY, LimitY.x, LimitY.y);
+        GetComponentInChildren<Transform>().eulerAngles = new Vector3(RotationY, RotationX, 0);
+    }
+}
---
+++ Captain Toad Easter Feaster Project/Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerScript : MonoBehaviour
+{
+    private CharacterController _characterController;
+ 
+    // Start is called before the first frame update
+
+    private Vector3 _velocity;
+
+    [Header("Locomotion Parameters")]
+    [SerializeField] private float _mass = 75; // [kg]
+
+    [SerializeField] private float _acceleration = 30; // [m/s^2]
+
+    [Header("Dependencies")]
+    [SerializeField, Tooltip("What should determine the absolute forward when a player presses forward.")]
+    private Transform _absoluteForward;
+
+    private Vector3 _movement;
+    private float _dragOnGround = 5;
+
+    void Start()
+    {
+        _characterController = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(_velocity.magnitude);
+        ApplyGravity();
+        ApplyGround();
+        ApplyMovement();
+        ApplyGroundDrag();
+
+        _characterController.Move(_velocity * Time.deltaTime);//or Time.FixedDeltaTime
+        if (_movement.magnitude > 0.3f)
+        {
+            transform.forward = -_movement;
+            GetComponentInChildren<Transform>().forward = -_movement;
+        }
+    }
+
+    private void ApplyGroundDrag()
+    {
+        if (_characterController.isGrounded)
+        {
+            _velocity = _velocity * (1 - Time.deltaTime * _dragOnGround);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        if(_characterController.isGrounded)
+        {
+            Vector3 xzAbsoluteForward = Vector3.Scale(_absoluteForward.forward, new Vector3(1, 0, 1));
+            Quaternion forwardRotation = Quaternion.LookRotation(xzAbsoluteForward);
+            Vector3 relativeMovement = forwardRotation * _movement;
+            _velocity += relativeMovement * _acceleration * Time.deltaTime; // F(= m.a) [m/s^2] * t [s]
+        }
+    }
+
+    private void ApplyGround()
+    {
+        if(_characterController.isGrounded)
+        {
+            _velocity -= Vector3.Project(_velocity, Physics.gravity.normalized); //kleine pressure om character op de grond te houden
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if(!_characterController.isGrounded)
+        {
+            _velocity += Physics.gravity * Time.deltaTime; //zwaartekracht adden
+        }
+    }
+}
